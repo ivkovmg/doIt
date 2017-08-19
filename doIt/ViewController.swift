a0a47d0cfafd6fbe0ad13bb6dff1d9d7fbefdd06
Matthew Ivkovich <ivkovmg@auburn.edu>
2017-08-19 00:16:30 -0400

    Setup table view

--- doIt/ViewController.swift
+++ doIt/ViewController.swift
@@ -8,18 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate , UITableViewDataSource {//ADD LAST TWO FOR TABLE VIEW
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self //MUST BE SET-UP FOR TABLEVIEW
+        tableView.delegate = self //MUST BE SET-UP FOR TABLE VIEW
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //THIS IS A TABLEVIEW FUNCTION ALREADY AVALIABLE IN SWIFT
+        return 10
     }
-
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //THIS IS A TABLEVIEW FUNCTION ALREADY AVALIABLE IN SWIFT
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Hello" //DON'T KNOW WHY THERE IS A ?
+        return cell
+    }
 }
 
